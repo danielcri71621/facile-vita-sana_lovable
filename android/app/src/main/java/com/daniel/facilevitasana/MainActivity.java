4fcb6a97bd5ec7b6bdcbf652e24616ccde07c844
user07 <daniel.cogozzo@gmail.com>
2025-06-20 23:22:56 +0200

    "refactor: renamed package and updated WebView URL"

---
+++ android/app/src/main/java/com/daniel/facilevitasana/MainActivity.java
@@ -0,0 +1,5 @@
+package com.daniel.facilevitasana;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,15 +1,9 @@
-
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.lovable.facilevitasana',
-  appName: 'facile-vita-sana',
-  webDir: 'dist',
-  bundledWebRuntime: false
-  // server: {
-  //   url: 'https://34756234-96f3-41d3-8f04-2f06aeaa2f38.lovableproject.com?forceHideBadge=true',
-  //   cleartext: true
-  // }
+  appId: 'com.daniel.myapp',
+  appName: 'facile vita sana',
+  webDir: 'dist'
 };
 
 export default config;
